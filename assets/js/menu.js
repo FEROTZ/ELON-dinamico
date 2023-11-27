8d475f4986f198c33ccf809f9d486befd216d243
Fernando Cortés <fernando.cortes971@gmail.com>
2023-11-26 19:35:53 -0600

    cambios de diseño Menu desplegable implementado

---
+++ assets/js/menu.js
@@ -0,0 +1,30 @@
+const hamburger = document.querySelector("#hamburger");
+const menu = document.querySelector("#menu");
+const closeMenu = document.querySelector("#closeMenu");
+
+hamburger.addEventListener("click", function () {
+    console.log("entró")
+    menu.classList.toggle("active");
+    hamburger.setAttribute("hidden", "");
+});
+
+// closeMenu.addEventListener("click", function () {
+//     menu.classList.remove("active");
+//     hamburger.removeAttribute("hidden");
+// });
+
+document.addEventListener("click", function (event) {
+    if(!menu.contains(event.target) && !hamburger.contains(event.target) || closeMenu.contains(event.target)) {
+        menu.classList.remove("active");
+        hamburger.removeAttribute("hidden");
+    }
+})
+
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 100) {
+        hamburger.classList.add("fixed");
+    } else {
+        hamburger.classList.remove("fixed");
+    }
+});
