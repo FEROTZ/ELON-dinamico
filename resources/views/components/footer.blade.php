f08ba72d88acca1ed260cef12018981c68440736
Fernando Cortés <fernando.cortes971@gmail.com>
2023-12-05 16:15:35 -0600

    [ADD] header and footer survey components

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+    <div class="d-sm-flex justify-content-center justify-content-sm-between">
+        <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © ELON Academy
+            2023</span>
+        <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"> Diseñado por <a
+                href="https://www.instagram.com/fernando.cortesg/" target="_blank">Fernando Cortés</a> </span>
+    </div>
+</footer>
---
+++ resources/views/components/survey-header.blade.php
@@ -0,0 +1,5 @@
+<nav class="navbar-elon p-0 fixed-top d-flex flex-row">
+    <div class="navbar-menu-wrapper flex-grow d-flex align-items-stretch justify-content-center">
+        <img src="{{url('/assets/images/logo-elon-x.png')}}" alt="" srcset="">
+    </div>
+</nav>
