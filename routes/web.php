c64dc07ab7d2c10a1650197821ea2fc0080245b3
Fernando Cortés <fernando.cortes971@gmail.com>
2023-12-05 18:47:55 -0600

    [ADD] rutas para ver registro de certificaciones

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\registerCertificationController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,12 +17,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified',])->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
 });
+
+Route::get('/register-certification', [registerCertificationController::class, 'index']);
+
+Rote::post('/register-certification', [registerCertificationController::class, 'store']);
